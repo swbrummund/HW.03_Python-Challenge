5ae81728b0d4c00c10f0d066db28ab27da01e532
Seth Brummund <swbrummund@gmail.com>
2019-07-30 18:22:45 -0500

    Tuesday Changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,48 +1,41 @@
 import os
 import csv
+from collections import Counter
 
 csv_path = os.path.join("budget_data.csv")
 
-# Average_Change = 0
-# Greatest_Increase = 0
-# Greatest_Decrease = 0
-# Date_I = "date"
-# Date_D = "date"
-
-with open(csv_path, newline="") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
-
-    next(csv_reader,None)
-
+def PyBank(Budget):
     Month = 0
-    Profit_Losses = 0
+    Profit_Losses = []
+    Average_List = []
 
-#Count the number of months
+    #Count the number of months
     for row in csv_reader:
         Month += 1
+        
+        #Add Profit/Losses to a list
+        Profit_Losses.append(float(row[1]))
 
-#Count Total Profit/Losses
-    for row in csv_reader:
-        Profit_Losses += float(row[1])
+        #Add Changes of each month to a list
+        a = row[1]
+        
 
-#Average Change Each Month
-    Average_Change = Profit_Losses/Month
-
-#Greatest Increase in Profits
-    Date_I = row[0]
-    Greatest_Increase = float(row[1])
-    for row in csv_reader:
+        #Greatest Increase in Profits
+        Date_I = row[0]
+        Greatest_Increase = float(row[1])
+        
         if float(row[1]) > Greatest_Increase:
             Greatest_Increase = float(row[1])
             Date_I = row[0]
         else:
             Greatest_Increase = Greatest_Increase
             Date_I = Date_I
-
-#Greatest Decrease in Profits
-    Greatest_Decrease = float(row[1])
-    Date_D = row[0]
-    for row in csv_reader:
+        
+        #Greatest Decrease in Profits
+        Greatest_Decrease = float(row[1])
+        Date_D = row[0]
+        
+        
         if float(row[1]) < Greatest_Decrease:
             Greatest_Decrease = float(row[1])
             Date_D = row[0]
@@ -50,10 +43,31 @@ with open(csv_path, newline="") as csv_file:
             Greatest_Decrease = Greatest_Decrease
             Date_D = Date_D
 
+    #Count Total Profit/Losses
+    #Average Change Each Month
+    Total_Profit_Losses = sum(Profit_Losses)
+    Average_Change = len(Profit_Losses)/86
+    
+   
+        
+
+   
+    
+
+    #print(Profit_Losses)
     print(f'Financial Analysis')
     print(f'------------------------')
     print(f'Total Months: {Month}')
-    print(f'Total Profit/Losses: ${Profit_Losses}')
+    print(f'Total Profit/Losses: ${Total_Profit_Losses}')
     print(f'Average Change: ${Average_Change}')
     print(f'Greatest Increase: {Date_I} with ${Greatest_Increase}')
     print(f'Greatest Decrease: {Date_D} with ${Greatest_Decrease}')
+
+with open(csv_path, newline="") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+
+    #print(csv_file)
+
+    next(csv_reader,None)
+
+    PyBank(csv_reader)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,12 @@
 import os
 import csv
+from collections import Counter
 
 csv_path = os.path.join("election_data.csv")
+
+Votes = 0
+Candidate_List = []
+Candidates = {}
 
 with open(csv_path, newline="") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
@@ -9,16 +14,43 @@ with open(csv_path, newline="") as csv_file:
     next(csv_reader,None)
 
 #Count total number of votes cast
-    Votes = 0  
     for row in csv_reader:
         Votes += 1
     print(f'Total Votes: {Votes}')
-#List of candidates who received votes
 
-#Percentage of votes each candidate receieved
+#List of candidates who received votes
+    for row in csv_reader:
+        Candidate_List.append(float(row[2]))
+
+    for candidate in Candidate_List:
+        if candidate in Candidates:
+            Candidates[candidate] += 1
+        else:
+            Candidates[candidate] = 1
+    print(Candidates)
 
 #Total number of votes each candidate received
+    Khan_Votes = 0
+    Correy_Votes = 0
+    Li_Votes = 0
+    OTooley_Votes = 0
 
+    for row in csv_reader:
+        if row[2] == 'Khan':
+            Khan_Votes += 1
+        elif row[2] == 'Correy':
+            Correy_Votes += 1
+        elif row[2] == 'Li':
+            Li_Votes += 1
+        elif row[2] == "O'Tooley":
+            OTooley_Votes += 1
+            
+#Percentage of votes each candidate receieved
+    Percent_Khan = Khan_Votes/Votes
+    Percent_Correy = Correy_Votes/Votes
+    Percent_Li = Li_Votes/Votes
+    Percent_OTooley = OTooley_Votes/Votes
+    
 #Winner of election based on popular vote
 
 #Print analysis to terminal
